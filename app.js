47f6d466b0778be1066033cdb57f91ca1ec03f53
Brian Argenti <brian.h.argenti@gmail.com>
2025-07-16 16:22:14 -0500

    Module 3 Completed Baseline

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // register handlebars partials
-handlebars.registerPartials(path.join(__dirname + '/app_server/views/partials'));
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('./travel', travelRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
